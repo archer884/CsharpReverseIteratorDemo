48806a224ac069157e4a890fe54d7e025a1ac684
J/A <jarcher@balglobal.com>
2018-11-20 10:54:17 -0600

    First cut

---
+++ demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Reverse;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = Enumerable.Range(1, 100);
+            var reversed = sequence.ReverseSeq();
+
+            foreach (var item in reversed)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ reverse/IteratorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse
+{
+    public static class IteratorExtensions
+    {
+        /// <summary>
+        /// Reverses an IEnumerable(T), returning the same values in reverse order
+        /// as IEnumerable(T).
+        /// </summary>
+        public static IEnumerable<T> ReverseSeq<T>(this IEnumerable<T> collection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
