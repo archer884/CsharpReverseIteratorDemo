86bfac648c4fb02155594e438eb31b8b72e267cd
J/A <jarcher@balglobal.com>
2018-11-20 11:22:00 -0600

    Lemme really fuck this up.

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Reverse;
 
 namespace demo
 {
     class Program
     {
+        static int Seconds = 1000; // Multiplier for milliseconds
+
+        static IEnumerable<int> LetMeReallyFuckThisUp()
+        {
+            Thread.Sleep(1 * Seconds);
+            for (var i = 1; i <= 100; ++i)
+                yield return i;
+        }
+
         static void Main(string[] args)
         {
-            var sequence = Enumerable.Range(1, 100);
+            var sequence = LetMeReallyFuckThisUp();
             var reversed = sequence.ReverseSeq();
 
             foreach (var item in reversed)
